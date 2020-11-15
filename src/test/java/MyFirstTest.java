c045d5ee28919c5a3f9fd030685d2978a02cd1f6
juliamax-cloud <juliamax@inbox.lv>
2020-11-15 14:32:39 +0200

    change in int sum

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Test;
 
-public class myfirsttest {
+public class MyFirstTest {
     @Test
     public void firsttest() {
         System.out.println("Hello corona!");
 
-        int sum = sumTwoDigits(15,45);
+        int sum = sumTwoDigits(20,45);
         System.out.println(sum);
         System.out.println(sumTwoDigits(9,13));
     }
