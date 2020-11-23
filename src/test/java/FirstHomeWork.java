350920e780ac80f511f36f48664564002bf663ce
juliamax-cloud <juliamax@inbox.lv>
2020-11-23 16:13:29 +0200

    change in int sum

--- src/test/java/FirstHomeWork.java
+++ src/test/java/FirstHomeWork.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 public class FirstHomeWork {
+    @Test
+    public void credit() {
+        double sum = sumAbDigits(1000,0.2);
+        System.out.println(sum);
+    }
+
+    private double sumAbDigits(int a, Double b) {
+        double c = a + a * b;
+        return c;
+    }
+    
 }
